a19ab1cc93284665dc3189853fd0723220006288
Kushagar <apna-github-kushagar.saklani47@gmail.com>
2025-08-24 22:29:18 +0530

    fix: cors and dynamic port

--- server.js
+++ server.js
@@ -37,6 +37,7 @@ app.post("/send", async (req, res) => {
   }
 });
 
-app.listen(5000, () => {
-  console.log("🚀 Backend running on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`🚀 Backend running on port ${PORT}`);
 });
